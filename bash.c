40decd75b2708ebd6403f0e3b6e58c24898a847b
Drose <davidroserocalle@gmail.com>
2019-08-28 18:31:31 +0000

    add function main, create files to create new proccess and the sys_error, fix problems whit betty

---
+++ bash.c
@@ -0,0 +1,44 @@
+#include "head.h"
+
+/**
+ * main - execute new shell
+ *
+ * Return: 0 to success.
+ */
+
+int main(void)
+{
+	char *buf = NULL;
+	char *str = NULL;
+	char **argv = NULL;
+	int argc = 0, characters = 0, count = 1;
+	size_t buf_size = 0;
+
+	while (EOF)
+	{
+		printf("#cisfun$ ");
+		fflush(stdin);
+		characters = getline(&buf, &buf_size, stdin);
+		if (characters  == -1)
+		{
+			fflush(stdin);
+			free(buf);
+			exit(1);
+		}
+		else
+		{
+			argc = new_argc(buf);
+			str = rm_enter(buf);
+			argv = new_argv(argc, str);
+
+			if (argc != 0)
+			{
+				argv[0] = add_path(argv[0], argv, argc, buf);
+				exec(argv, buf, argc, count);
+			}
+			count++;
+		}
+	}
+	free(buf);
+	return (0);
+}
--- gen_authors.sh
+++ gen_authors.sh
@@ -7,8 +7,6 @@ set -e
 
 {
 	cat <<-'EOH'
-	# This file lists all individuals having contributed content to the repository.
-	# For how it is generated, see `hack/generate-authors.sh`.
 	EOH
 	echo
 	git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf
